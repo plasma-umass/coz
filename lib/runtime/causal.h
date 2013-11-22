dabdd0515fa5a92d373400cdd6aea8ac42d97538
Charlie Curtsinger <charlie@cs.umass.edu>
2013-11-22 12:51:22 -0500

    Uncommented slowdown code

--- lib/runtime/causal.h
+++ lib/runtime/causal.h
@@ -35,17 +35,14 @@ private:
 	
 	void profilerThread() {
 		while(true) {
-			//Host::wait(10 * Time::ms);
-			//DEBUG("Here");
-			
-			// Find a block that appear to be executing
+			// Find a block that appears to be executing
 			uintptr_t block = CausalEngine::getActiveBlock(100 * Time::ms);
 			
 			// Run a slowdown experiment on the chosen block
 			_slowdown_results[block] += CausalEngine::runSlowdown(block, 50 * Time::ms, 500 * Time::us);
 			
 			// Run a speedup experiment on the chosen block
-			//_speedup_results.emplace(b, CausalEngine::runSpeedup(b, 50 * Time::ms, (rand() % 10000 + 500) * Time::us));
+			_speedup_results.emplace(b, CausalEngine::runSpeedup(b, 50 * Time::ms, (rand() % 10000 + 500) * Time::us));
 			
 			// Get the baseline progress rate
 			_baseline += CausalEngine::runBaseline(50 * Time::ms);
