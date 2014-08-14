5009d70cb29800bb9fa0b4b88ba02ea10cc8d21f
Charlie Curtsinger <charlie@cs.umass.edu>
2014-08-14 14:11:41 -0400

    No more pointless siglock

--- include/causal/thread_state.h
+++ include/causal/thread_state.h
@@ -3,7 +3,6 @@
 
 #include <atomic>
 
-#include "causal/siglock.h"
 #include "causal/timer.h"
 
 class thread_state {
@@ -23,65 +22,14 @@ public:
   bool check_in_use() {
     return in_use;
   }
-    
-  
-  class ref {
-  public:
-    ref(thread_state* s, siglock::context c) {
-      if(s->_l.lock(c)) {
-        _s = s;
-      } else {
-        _s = nullptr;
-      }
-    }
-    
-    ref(ref&& other) {
-      _s = other._s;
-      other._s = nullptr;
-    }
-    
-    ~ref() {
-      if(_s != nullptr) {
-        _s->_l.unlock();
-      }
-    }
-    
-    inline void operator=(ref&& other) {
-      _s = other._s;
-      other._s = nullptr;
-    }
-    
-    inline operator bool() {
-      return _s != nullptr;
-    }
-    
-    inline thread_state* operator->() {
-      return _s;
-    }
-    
-  private:
-    ref(const ref&) = delete;
-    void operator=(const ref&) = delete;
-    
-    thread_state* _s;
-  };
   
   /**
-  * Get the thread local state in a reference. The `force` parameter should only be
-  * true when in error-handling mode. This makes it possible to collect a backtrace, which
-  * calls pthread_mutex_lock.
+  * Get state for the current thread
   */
   static thread_state& get() {
     static thread_local thread_state s;
     return s;
-    //return thread_state::ref(&s, c);
   }
-  
-private:
-  friend class ref;
-  
-  /// A siglock used to ensure access to thread state is atomic
-  siglock _l;
 };
 
 #endif
