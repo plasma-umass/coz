d9ff3442a589462dae0c34b68f3867203c0a8f50
Charlie Curtsinger <charlie@cs.umass.edu>
2014-09-12 16:36:15 -0400

    Slightly more helpful error message

--- tools/coz-plot/coz-plot.r
+++ tools/coz-plot/coz-plot.r
@@ -5,6 +5,14 @@ require(plyr)
 
 args <- commandArgs(trailingOnly = TRUE)
 
+check_valid_data <- function(dat) {
+  print(dat)
+  if(length(dat$line) == 0) {
+    print("There are no samples left to plot. You need more runs to collect a usable profile. Make sure your progress points are being executed.")
+    q()
+  }
+}
+
 filename <- 'stdin'
 if(length(args) == 1) {
   filename <- args[1]
@@ -12,6 +20,8 @@ if(length(args) == 1) {
 
 # Read the CSV file
 dat <- read.csv(filename, sep='\t')
+
+check_valid_data(dat)
 
 # Compute the number of points for each line
 dat$points <- tabulate(dat$line)[dat$line]
@@ -22,6 +32,8 @@ dat <- subset(dat, points >= 10)
 # Remove points above 1.0 or below -1.0
 dat <- subset(dat, counter_speedup < 1.0)
 dat <- subset(dat, counter_speedup > -1.0)
+
+check_valid_data(dat)
 
 # Compute the slope of each line's regression line
 slopes <- daply(dat, .(line), function(x) {
